d98a19a457727731650cd843fc97f2f2105cfa78
MuthuGSubramanain <muthu.g.subramanian@outlook.com>
2020-02-07 08:15:16 +0530

    version 1.0

--- TD_SF_Tool/macro_converter.py
+++ TD_SF_Tool/macro_converter.py
@@ -12,134 +12,170 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def create_macro(k,v,block):
+def create_macro(script):
     try:
-        rm_space = re.compile(r'\s+')
-        pd = list(filter(lambda x: k in re.sub(rm_space, ' ', x), block))[0].split('as', 1)
-        mac_name = ''.join(list(filter(lambda x: k in re.sub(rm_space, ' ', x), pd))).split('macro')[-1].replace('\n', '')
-        macro_name = mac_name.split('.')
-        converted = v.format(macro_name[0].strip().upper(),macro_name[1].strip().upper())
-        logging.info('create block converted')
-        return converted
+        if 'macro' or 'as' in script:
+            name_extract = script.partition("macro")[2].partition("as")[0].strip().upper().split('.', 1)
+            create_block = conf_map['create macro'].format(name_extract[0], name_extract[1])
+            logging.info('create block converted')
+            return create_block
+        else:
+            return 'None'
     except Exception as err:
-        logging.info(err)
+        logging.error(err)
 
-def replace_macro(k,v,block):
-    try:
-        rm_space = re.compile(r'\s+')
-        pd = list(filter(lambda x: k in re.sub(rm_space, ' ', x), block))[0].split('as', 1)
-        mac_name = ''.join(list(filter(lambda x: k in re.sub(rm_space, ' ', x), pd))).split('macro')[-1].replace('\n', '')
-        macro_name = mac_name.split('.')
-        converted = v.format(macro_name[0].strip().upper(),macro_name[1].strip().upper())
-        logging.info('replace block converted')
-        return converted
-    except Exception as err:
-        logging.info(err)
 
-def merge_into(k,v,block):
+def merge_into(res):
     try:
-        merge = k.split('\n')
-        merge_block = []
-        for i, ite in enumerate(merge):
-            if re.findall('shiftstartdatetime', ite):
-                col_name = ite.split('+')[0].strip()
-                field = re.search(r'cast([^/]+)', ''.join(ite.split('+')[1])).group(1).replace('(','').strip()
-                update_col = conf_map['date_add'].format(field,col_name) + ' as ' +field +'_ts ,'
-                logging.info(update_col)
-                merge_block.append(update_col+'\n')
-            else:
-                merge_block.append(ite+'\n')
-        logging.info('merge block completed')
-        blocks = []
+        if 'merge' in res:
+            merge_var = '{' + 'sqlText:{0}'.format('var_sql_merge_base') + '}'
+            merge_script = res.partition("merge")[1] + res.partition("merge")[2].partition(";")[0]
+            exec_sf = conf_map['sf_exe_m'].format(merge_var)
+            merge_block = []
+            merge = merge_script.split('\n')
+            for i, ite in enumerate(merge):
+                if re.findall('shift_info.shiftstartdatetime', ite):
+                    col_name = ite.split('+')[0].strip()
+                    field = re.search(r'cast([^/]+)', ''.join(ite.split('+')[1])).group(1).replace('(','').strip()
+                    update_col = conf_map['date_add'].format(field,col_name) + ' as ' +field +'_ts ,'
+                    logging.info(update_col)
+                    merge_block.append(update_col+'\n')
+
+                else:
+                    merge_block.append(ite+'\n')
+            merge_data = conf_map['merge_into'].format(''.join(merge_block)) + exec_sf
+            logging.info('merge block completed')
+            return merge_data,merge_script
+        else:
+            return ['None','None']
+    except Exception as error:
+        logging.error(error)
+
+
+def insert_block(script):
+    if 'insert' in script and script.count('insert')==1:
+        insert_var = '{' + 'sqlText:{0}'.format('var_sql_insert_base') + '}'
+        insert_extract = script.partition("insert")[1]+script.partition("insert")[2].partition(");")[0]
+        insert_script = conf_map['insert_into'].format(insert_extract) + conf_map['sf_exe_m'].format(insert_var)
+        return insert_script
+    else:
+        return 'None'
+
+def select_block(script):
+    if 'select' in script and script.count('select')==1:
+        select_var = '{' + 'sqlText:{0}'.format('var_sql_select_base') + '}'
+        select_extract = script.partition("select")[1]+script.partition("select")[2].partition(");")[0]
+        select_script = conf_map['select'].format(select_extract)+ conf_map['sf_exe_m'].format(select_var)
+        return select_script
+    else:
+        return 'None'
+
+
+def update_block(block):
+    try:
         if re.findall('update', block):
-            blocks.append('\nvar_sql_logical_delete_capture = ' +"`" + 'update' + block.split('from',1)[0].split('update',1)[-1])
-            logging.info('update block')
-        if re.findall('set',block):
-            blocks.append('set' + block.split('from',1)[-1].split('set',1)[-1].split('where',1)[0])
-            logging.info('set block')
-        if re.findall('from',block):
-            blocks.append(' from'+ block.split('from',1)[-1].split('set',1)[0] )
-            logging.info('from block')
-        if re.findall('where',block):
-            blocks.append('where' +block.split('from',1)[-1].split('set',1)[-1].split('where',1)[-1].split(');')[0]+ "`;")
-            logging.info('where block')
-        as_block = conf_map['as_block']
-        merge_sec = v.format(''.join(merge_block))
-        exe_macro = conf_map['sf_exe_m']
-        ot_blocks = ''.join(blocks)
-        exe_ot = conf_map['sf_exe_b']
-        end =  conf_map['sf_exe_e']
-        converted = '\n'+op_log+as_block+ merge_sec + ';' +'\n\n'+ exe_macro + '\n\n'+ ot_blocks+'\n'+exe_ot+'\n\n'+ end
-        return converted
-    except Exception as err:
-        logging.info(err)
 
-def new_keys(block):
-    keyw = []
-    inp_keys = []
-    resp_keys = []
-    for i in block:
-        keyw.append(i.split(' ', 1)[0] + ' macro')
-    for kys in list(conf_map.keys()):
-        inp_keys.append(" ".join(kys.split()))
-    for h in keyw:
-        if h not in inp_keys:
-            resp_keys.append(list(filter(lambda x: h in x, block)))
-    with open('td_rs_new_keys.txt', 'w') as f:
-        for item in resp_keys:
-            f.write("%s\n" % item)
-    return resp_keys
+            update = block.partition("update")[1] + block.partition("update")[2].partition("from")[0]
+            update_script = update
+            logging.info('update block')
+            return update_script
+        else:
+            return 'None'
+    except Exception as error:
+        logging.error(error)
+
+def set_block(block):
+    try:
+        if re.findall('set',block):
+            set_script = block.partition("set")[1] + block.partition("set")[2].partition("where")[0]
+            logging.info('set block')
+            return set_script
+        else:
+            return 'None'
+    except Exception as error:
+        logging.error(error)
+
+def from_block(block):
+    try:
+        if re.findall('from',block):
+            from_script = block.partition("from")[1] + block.partition("from")[2].partition("where")[0]
+            logging.info('from block')
+            return from_script
+        else:
+            return 'None'
+    except Exception as error:
+        logging.error(error)
+
+def where_block(block):
+    try:
+        if re.findall('where',block):
+            where_script = block.partition("where")[1] + block.partition("where")[2].partition(");")[0]
+            logging.info('where block completed')
+            return where_script
+        else:
+            return 'None'
+    except Exception as error:
+        logging.error(error)
 
 def query_processor(cont,file_name,op_path):
     try:
         rm_space = re.compile(r'\s+')
-        mac = cont
+        mac = cont.lower()
         cleaned = []
         logging.info('initated conversion for '+file_name)
         for items in mac.split(' '):
-            if len(items) >=1:
+            if items.startswith('--' or '\n--'):
+                cleaned.append('//'+items)
+            else:
                 cleaned.append(items)
-        clean = ' '.join(cleaned)
-        block = ''.join(clean.split(');')).replace('\n','\n ').replace('"','').split(';')
+        script = ' '.join(cleaned)
+        rest_script = ''
         op_file_name = file_name + '_converted.txt'
-        query_resp = []
-        merge = 'merge' + mac.split('merge',1)[1].split(');',1)[0]
-        non_merge = mac.split('merge',1)[1].split(');',1)[1]
-        for k, v in conf_map.items():
-            if k == 'create macro':
-                if list(filter(lambda x: 'create macro' in re.sub(rm_space, ' ', x), block)):
-                    c_macro = create_macro(k, v,block)
-                    query_resp.append(c_macro)
-            elif k == 'replace macro':
-                if list(filter(lambda x: 'replace macro' in re.sub(rm_space, ' ', x), block)):
-                    c_macro = replace_macro(k, v,block)
-                    query_resp.append(c_macro)
-            elif k == 'merge into':
-                m_macro = merge_into(merge,v,non_merge)
-                query_resp.append(m_macro)
+        sf_script = []
+        if 'as' in script:
+            script_block = script.split('as', 1)
+            create = script_block[0]
+            rest_script = ''.join(script_block[1:])
+            create_block = create_macro(create)
+            sf_script.append(create_block)
+            sf_script.append(conf_map['as_block'])
+        if rest_script.replace('\n','').replace('\t','').replace('\r','').startswith('(select' or 'select'):
+            select_script = select_block(rest_script)
+            sf_script.append(select_script + '\n')
+        if rest_script.replace('\n','').replace('\t','').replace('\r','').startswith('(insert' or 'insert'):
+            insert_script = insert_block(rest_script)
+            sf_script.append(insert_script + '\n')
+        elif 'BEGIN TRANSACTION'.lower() in rest_script:
+            no_script = conf_map['no_into'].format(rest_script)
+            sf_script.append(no_script + '\n')
+        merge_block = merge_into(rest_script)
+        sf_script.append(merge_block[0] + '\n')
+        after_merge_script = rest_script.replace(merge_block[1], '')
+        update_collection = []
+        update = update_block(after_merge_script)
+        update_var = '{' + 'sqlText:{0}'.format('var_sql_update_base') + '}'
+
+        update_collection.append(update)
+        after_update_script = after_merge_script.replace(update, '')
+        set_script = set_block(after_update_script)
+        update_collection.append(set_script + '\n')
+        after_set_script = after_update_script.replace(set_script, '')
+        from_script = from_block(after_set_script)
+        update_collection.append(from_script + '\n')
+        after_from_script = after_set_script.replace(from_script, '')
+        where_script = where_block(after_from_script)
+        update_collection.append(where_script + '\n')
+        update_script = conf_map['update'].format(''.join(update_collection))
+        sf_script.append(after_from_script)
+        sf_script.append(update_script)
+        update_exe = conf_map['sf_exe_update'].format(update_var)
+        sf_script.append('\n'+update_exe)
         with open(str(op_path)+'/'+op_file_name , 'w') as f:
-            for item in query_resp:
+            sf_script.append(conf_map['sf_exe_e'])
+            sf_converted = [value for value in sf_script if value != 'None\n']
+            for item in sf_converted:
                 f.write("%s" % item)
             logging.info('completed conversion for ' + op_file_name)
             logging.info('^'*80)
-        new_keys(block)
     except Exception as error:
         logging.info(error)
-
-# if __name__ == '__main__':
-#     #
-#     root = Tk()
-#     root.withdraw()
-#     src_folder_selected = filedialog.askdirectory(title = 'Choose the Source Directory')
-#     dest_folder_selected = filedialog.askdirectory(title = 'Choose the destination Directory')
-#     src_path = "C:\\Users\\45444\\PycharmProjects\\TD_FS_migrator\\files\\extracted\\"
-#     op_path = "C:\\Users\\45444\\PycharmProjects\\TD_FS_migrator\\files\\converted\\"
-#     inp_list = os.listdir(src_folder_selected)
-#     for files in inp_list:
-#         file = src_folder_selected+'\\'+files
-#         query_processor(file,dest_folder_selected)
-
-    # logging.info('-'*20)
-    # query_processor(r'C:\Users\45444\PycharmProjects\TD_FS_migrator\files\extracted\create macro edw_stage_016.m_lh2_shift_state_b.txt',op_path)
-
-# ''.join(op[op.index('using \n'):op.index('when matched then \n')]).replace('\n','').replace('\t','')
--- TD_SF_Tool/macro_coversion_config.py
+++ TD_SF_Tool/macro_coversion_config.py
@@ -1,13 +1,19 @@
 conf_map = {
-    'create macro': '''CREATE OR REPLACE PROCEDURE "{0}"."{1}"()\nRETURNS VARCHAR(16777216)\nLANGUAGE JAVASCRIPT\nSTRICT\nEXECUTE AS OWNER''',
-    'replace macro': '''CREATE OR REPLACE PROCEDURE "{0}"."{1}"()\nRETURNS VARCHAR(16777216)\nLANGUAGE JAVASCRIPT\nSTRICT\nEXECUTE AS OWNER''',
-    'merge into':   "var_sql_merge_base = `{0})`",
+    'create macro': '''CREATE OR REPLACE PROCEDURE "{0}"."{1}"()
+RETURNS VARCHAR(16777216)
+LANGUAGE JAVASCRIPT
+STRICT
+EXECUTE AS OWNER\n\n''',
+    'as_block':  'AS'+'\n' +"$$"+ '\n' +'try {' ,
+    'merge_into':   "var_sql_merge_base = `{0})`",
+    'insert_into':   "var_sql_insert_base = `{0})`",
+    'update':   "var_sql_update_base = `{0})`",
+    'no_into':   "var_sql_base = `{0})`",
+    'select': "var_sql_select_base = `{0})`",
     'date_add': "dateadd('second', {0}, {1})",
-    'as_block':  'AS'+'\n' +" $$"+ '\n' +'try {' ,
-    'sf_exe_m' : '''snowflake.execute( {sqlText: var_sql_merge_base } ); ''',
-
-    'sf_exe_b' : '''snowflake.execute( {sqlText: var_sql_logical_delete_capture } ); ''',
-
+    'sf_exe_m' : '''\n\nsnowflake.execute( {0} ); ''',
+    'sf_exe_update' : '''\n\nsnowflake.execute( {0} ); ''',
+    'sf_exe_delete' : '''snowflake.execute( {sqlText: var_sql_logical_delete_capture } ); ''',
     'sf_exe_e' : ''' }\ncatch (err)
      {
      return "Failed: " + err;   
